ab2571e12d011e3324259d4f4e3023d106f6d309
a-rmz <armzprz@gmail.com>
2016-11-03 09:21:24 -0600

    Test to try Jenkins

---
+++ src/test/java/JenkinsTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by a-rmz on 11/3/16.
+ */
+public class JenkinsTest {
+
+    @Test
+    public void jenkinsTest() {
+        Assert.assertTrue(true);
+    }
+
+
+}
