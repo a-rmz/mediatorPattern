e4ecbddd698f0d1f131d32d3237fb8f81e0435eb
a-rmz <armzprz@gmail.com>
2016-11-10 07:34:25 -0600

    Base interfaces with methods

---
+++ src/main/java/Aircrafts/Aircraft.java
@@ -0,0 +1,13 @@
+package Aircrafts;
+
+/**
+ * Created by a-rmz on 11/10/16.
+ */
+
+// Colleague interface
+public interface Aircraft {
+
+    public void receibeMessage();
+    public void sendMessage(String message);
+
+}
