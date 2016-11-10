c561de0a29a4ca4aa60857c5444241ae1a7569c3
a-rmz <armzprz@gmail.com>
2016-11-10 08:24:48 -0600

    Main with tests

---
+++ src/main/java/mx/iteso/mediator/mediatorMain.java
@@ -0,0 +1,27 @@
+package mx.iteso.mediator;
+
+import mx.iteso.mediator.aircrafts.Aircraft;
+import mx.iteso.mediator.aircrafts.impl.Airplane;
+import mx.iteso.mediator.commandCenter.Mediator;
+import mx.iteso.mediator.commandCenter.impl.ControlTower;
+
+/**
+ * Created by a-rmz on 11/3/16.
+ */
+public class mediatorMain {
+
+    public static void main(String[] args) {
+        Mediator tower = new ControlTower();
+
+        Aircraft boeing = new Airplane(tower, "Boeing 747");
+        Aircraft airbus = new Airplane(tower, "Airbus A380");
+        Aircraft airforce = new Airplane(tower, "AirForce 1");
+
+        tower.registerAircraft(boeing);
+        tower.registerAircraft(airbus);
+        tower.registerAircraft(airforce);
+
+        boeing.sendMessage("I'm leaving. Don't arrive yet.");
+    }
+
+}
